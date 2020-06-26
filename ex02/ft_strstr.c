0d0138f4aae587beb17e4aa6835893c638086595
UNathi Khoba <ukhoba@c4r10s1.wethinkcode.co.za>
2020-06-26 12:02:15 +0200

    ex02 incomplete

--- ex02/ft_strstr.c
+++ ex02/ft_strstr.c
@@ -1,42 +1,36 @@
-#include <unistd.h>
-#include <stdlib.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	if	(*str != '\0')
-	{
-		ft_putchar(*str);
-		str++;
-		ft_putstr(str);
-	}
-}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ukhoba <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 12:01:11 by ukhoba            #+#    #+#             */
+/*   Updated: 2020/06/26 12:01:15 by ukhoba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	char	*substr;
+	char	*tmp_substr;
 
 	substr = (char*)malloc(sizeof*substr);
-
 	while	(*str != '\0')
 	{
-		if	(*str == *to_find && (str + 1) == (to_find + 1))
+		tmp_substr = (char*)malloc(sizeof*tmp_substr);
+
+		while	(*str == *to_find && *to_find != '\0')
 		{
 			*substr = *str;
+			str++;
+			to_find++;
 			substr++;
+			//ft_strstr(str, to_find);
 		}
+		
 		str++;
 	}
 
 	return substr;
-}
-
-int	main(void)
-{
-	ft_putstr(ft_strstr("Hey I am not what you looking for!", "I am"));
-	return 0;
 }
